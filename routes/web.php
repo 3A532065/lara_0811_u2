e0ff86f6c6ec153eae64c81357ebf7e21e263dfa
composer-lab-assets <weyna1649@gmail.com>
2018-10-30 18:02:48 +0800

    練習4-1, 2: 為route命名

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ Route::get('/', function () {
 
 });
 
-Route::get('hello/{name}', function ($name) {
-    return 'Hello, '.$name;
+Route::get('hello/{name?}', ['as'=>'hello.index', function ($name='Everybody') {
 
-});
+    return 'Hello, '.$name;
+}]);
+
 
 
 
