23503db25d85e3b26c7cc269388388c15e3a0c9a
composer-lab-assets <weyna1649@gmail.com>
2018-10-30 18:07:11 +0800

    練習5: 設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,17 @@ Route::get('say/{name?}', ['as'=>'hello.index', function ($name='Everybody') {
 
     return 'Hello, '.$name;
 }]);
+Route::get('dashboard', function () {
+    return 'dashboard';
+});
+Route::group(['prefix'=>'admin'], function () {
+    Route::get('dashboard', function () {
+        return 'admin dashboard';
+
+    });
+
+});
+
 
 
 
